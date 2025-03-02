8a250524f026caf79856ebea1d1bc37c55cbca33
valentine231 <ugwuvalentine519@gmail.com>
2025-03-02 02:19:20 +0100

    added Auth

--- src/Component/Loading.jsx
+++ src/Component/Loading.jsx
@@ -6,7 +6,7 @@ import {motion} from 'framer-motion';
 const Loading = () => {
   return (
     <div className="flex items-center justify-center space-x-2 ">
-        {[0,1,2,3].map((i)=>(
+        {[0,1,2].map((i)=>(
               <motion.div
               key={i}
          className='flex items-center justify-center shadow-lg  w-3 h-3 bg-blue-500 rounded-full'
@@ -16,7 +16,7 @@ const Loading = () => {
         }}
          transition={{duration: 0.7, repeat: Infinity, ease: 'easeInOut', delay:i*0.2}}
          >
-          ...
+          
          </motion.div>
         ))}
        
